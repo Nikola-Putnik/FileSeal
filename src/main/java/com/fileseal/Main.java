e81ed199f283baaa86d3a9346440ef092223c224
Nikola-Putnik <nikola.putnik@student.uclouvain.be>
2025-08-21 13:32:35 +0200

    Initial project structure with basic GUI setup

---
+++ src/main/java/com/fileseal/Main.java
@@ -0,0 +1,9 @@
+package com.fileseal;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new MainWindow());
+    }
+}
---
+++ src/main/java/com/fileseal/MainWindow.java
@@ -0,0 +1,12 @@
+package com.fileseal;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        setTitle("FileSeal");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
